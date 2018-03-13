dae3d5f062df9b7558ecc083b8dc72deeff429ef
BOLLECKER Théo <bolleckertheo@MacBook-Pro-de-BOLLECKER.local>
2018-03-13 10:35:44 +0100

    modify script

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 			<a href="index.php">Krowdis</a>
 		</div>
 		<nav class="kd-nav">
-			<a href="index.php">Accueil</a><<<<<<< HEAD
+			<a href="index.php">Accueil</a>
 			<a href="#kd-perso" class="js-scrollTo">Notre Solution</a>
 			<a href="usecase.php">Cas d'utilisation</a>
 <!-- 			<a href="#">Blog</a>
