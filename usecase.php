b1f18adc819e7a301a407c0a2e2d13355906f7a9
BOLLECKER Théo <bolleckertheo@MacBook-Pro-de-BOLLECKER.local>
2018-03-13 10:51:55 +0100

    usercase part + new image CE

--- usecase.php
+++ usecase.php
@@ -32,6 +32,26 @@
 			<img class="kd-case-img" src="assets/img/beelysnb.png">
 		</div>
 	</section>
+	<section>
+			<img src="assets/img/femmeCE.png" alt="Femme Caisse d'Epargne">
+			<h1>SPONSORING : LA CAISSE D’ÉPARGNE RHÔNE ALPES VALORISE SON SPONSORING ASVEL</h1>
+		<p>Le challenge :</p>
+		<p>Amplifier la notoriété de la Caisse d’Epargne Rhône Alpes comme sponsor du club de basket.
+			La banque sponsorise le club de basketball lyonnais et souhaite impliquer les fans dans sa communication pour augmenter sa visibilité,
+			l’engagement autour de son sponsoring et consolider un sentiment d’appartenance autour de sa marque via son sponsoring.</p>
+		<p>La solution :</p>
+		Krowdis a pour mission de digitaliser le canal marketing le plus puissant n’ayant jamais existé :
+		le bouche à oreille. Pour répondre aux challenges de la Caisse d’Epargne Rhône Alpes, Krowdis a mis à disposition de la banque une application mobile, qui, sous forme de jeu, propose à tous de devenir influenceur pour une marque, pour une association ou un club sportif. En donnant aux supporters la possibilité de créer, facilement, du contenu pour leur club préféré, la Caisse d’Epargne Rhône Alpes provoque un sentiment d’appartenance autour de sa marque. Une fois créées, les créations des fans sont partagées dans leurs propres communautés d’amis.
+		Les communautés touchées voient ainsi un contenu de
+		marque, très visuel, sur lequel figure un ami,
+		partagé par celui-ci. Et non un contenu sponsorisé diffusé par une marque sur lequel figure une star ou un inconnu. Pour mener à bien cette campagne, la Caisse d’Epargne Rhône Alpes et la startup ont mis en place une opération sur 3 matchs de l’ASVEL à domicile. Les supporters se sont mis en scène selon trois thématiques successives : élection du meilleur supporter de l’équipe, un challenge entre les fans, le staff du club et le sponsor, puis une campagne pour exprimer ses impressions sur le match en direct.
+		Les photos des fans les plus partagées et « aimées » ont été récompensées avec des lots. L’opération a par ailleurs été relayée sur les écrans géants du stade de l’ASVEL, sur les réseaux sociaux de la banque et du club et lors d’une opération de street-marketing.</p>
+		<p>Les résultats :</p>
+		<p><span>Nombre de photos :</span> Nous avons triplé le nombre de photos créées lors de cette opération par rapport aux opérations classiquement montées lors de jeux concours Facebook via une agence. L’aspect gaming de notre solution et la création d’une campagne qui intéresse le public expliquent notamment ce chiffre élevé.
+		<span>Temps passé sur les contenus :</span> Notre outil est branché sur les API Facebook et Google qui remontent un temps de session supérieur à 7mn.
+		<span>En route vers une opération de grande ampleur :</span> La Caisse d’Epargne Rhône Alpes renouvelle sa confiance pour valoriser son sponsoring sportif Olympique Lyonnais. Opération prévue à partir de mi-avril !
+		Zone de message</p>
+	</section>
 	<?php
 		include('footer.php');
 	?>
