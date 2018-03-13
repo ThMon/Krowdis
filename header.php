97ceeaef2bbffd974fef10d6f5c2b57f3ca52955
Thibaut <thibaut.monesma@gmail.com>
2018-03-13 11:39:46 +0100

    V1

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		</div>
 		<nav class="kd-nav">
 			<a href="index.php">Accueil</a>
-			<a href="#kd-perso" class="js-scrollTo">Notre Solution</a>
+			<!--<a href="#kd-perso" class="js-scrollTo">Notre Solution</a>-->
 			<a href="usecase.php">Cas d'utilisation</a>
 <!-- 			<a href="#">Blog</a>
  -->	<a href="form.php">Démo</a>
--- usecase.php
+++ usecase.php
@@ -13,10 +13,10 @@
 		<article class="kd-user-block bg-w">
 			<p>SPONSORING</p>
 		</article>
-		<article class="kd-user-block bg-y">
+		<article class="kd-user-block bg-y c-w">
 			<p>Plus d'information à venir</p>
 		</article>
-		<article class="kd-user-block bg-y">
+		<article class="kd-user-block bg-y c-w">
 			<p>RSE</p>
 		</article>
 		<article class="kd-user-block bg-w">
@@ -32,25 +32,41 @@
 			<img class="kd-case-img" style="width: 6%;" src="assets/img/beelysnb.png">
 		</div>
 	</section>
-	<section>
-			<img src="assets/img/femmeCE.png" alt="Femme Caisse d'Epargne">
-			<h1>SPONSORING : LA CAISSE D’ÉPARGNE RHÔNE ALPES VALORISE SON SPONSORING ASVEL</h1>
-		<p>Le challenge :</p>
+	<section class="kd-case-ce">
+		<img class="kd-img-case-left" src="assets/img/femmeCE.png" alt="Femme Caisse d'Epargne">
+		<h1>SPONSORING : LA CAISSE D’ÉPARGNE RHÔNE ALPES VALORISE SON SPONSORING ASVEL</h1>
+		<h2>Le challenge :</h2>
 		<p>Amplifier la notoriété de la Caisse d’Epargne Rhône Alpes comme sponsor du club de basket.
 			La banque sponsorise le club de basketball lyonnais et souhaite impliquer les fans dans sa communication pour augmenter sa visibilité,
 			l’engagement autour de son sponsoring et consolider un sentiment d’appartenance autour de sa marque via son sponsoring.</p>
-		<p>La solution :</p>
-		Krowdis a pour mission de digitaliser le canal marketing le plus puissant n’ayant jamais existé :
+		<h2>La solution :</h2>
+		<p>Krowdis a pour mission de digitaliser le canal marketing le plus puissant n’ayant jamais existé :
 		le bouche à oreille. Pour répondre aux challenges de la Caisse d’Epargne Rhône Alpes, Krowdis a mis à disposition de la banque une application mobile, qui, sous forme de jeu, propose à tous de devenir influenceur pour une marque, pour une association ou un club sportif. En donnant aux supporters la possibilité de créer, facilement, du contenu pour leur club préféré, la Caisse d’Epargne Rhône Alpes provoque un sentiment d’appartenance autour de sa marque. Une fois créées, les créations des fans sont partagées dans leurs propres communautés d’amis.
 		Les communautés touchées voient ainsi un contenu de
 		marque, très visuel, sur lequel figure un ami,
 		partagé par celui-ci. Et non un contenu sponsorisé diffusé par une marque sur lequel figure une star ou un inconnu. Pour mener à bien cette campagne, la Caisse d’Epargne Rhône Alpes et la startup ont mis en place une opération sur 3 matchs de l’ASVEL à domicile. Les supporters se sont mis en scène selon trois thématiques successives : élection du meilleur supporter de l’équipe, un challenge entre les fans, le staff du club et le sponsor, puis une campagne pour exprimer ses impressions sur le match en direct.
 		Les photos des fans les plus partagées et « aimées » ont été récompensées avec des lots. L’opération a par ailleurs été relayée sur les écrans géants du stade de l’ASVEL, sur les réseaux sociaux de la banque et du club et lors d’une opération de street-marketing.</p>
-		<p>Les résultats :</p>
+		<h2>Les résultats :</h2>
 		<p><span>Nombre de photos :</span> Nous avons triplé le nombre de photos créées lors de cette opération par rapport aux opérations classiquement montées lors de jeux concours Facebook via une agence. L’aspect gaming de notre solution et la création d’une campagne qui intéresse le public expliquent notamment ce chiffre élevé.
 		<span>Temps passé sur les contenus :</span> Notre outil est branché sur les API Facebook et Google qui remontent un temps de session supérieur à 7mn.
 		<span>En route vers une opération de grande ampleur :</span> La Caisse d’Epargne Rhône Alpes renouvelle sa confiance pour valoriser son sponsoring sportif Olympique Lyonnais. Opération prévue à partir de mi-avril !
 		Zone de message</p>
+	</section>
+	<section class="kd-case-univ">
+		<img class="kd-img-case-right" src="assets/img/UDL.png" alt="Université de Lyon">
+		<h1>SPONSORING : La Fondation pour l’Université de Lyon pousse les étudiants à entreprendre et le fait savoir. </h1>
+		<h2>Le challenge :</h2>
+		<p>Faire connaître la marque Beelys, l’entité de la FPUL qui accompagne les étudiants entrepreneurs. 
+		La Fondation Université de Lyon souhaite augmenter la notoriété de la marque Beelys dans les universités et grandes écoles de la région lyonnaise. 
+		Le but de la Fondation est non seulement d’augmenter la visibilité de ses contenus mais aussi d’intéresser le maximum d’étudiants.</p>
+		<h2>La solution :</h2>
+		<p>Krowdis organise un challenge entre les associations étudiantes des universités et écoles concernées sur une application dédiée.
+		Parce que monter un projet associatif est un premier pas vers l’entrepreneuriat, et parce que qui mieux que les étudiants eux-mêmes peuvent promouvoir un dispositif créé pour eux, la FPUL et Krowdis ont organisé un grand challenge inter-associations étudiantes. 
+		Les étudiants se sont mis en scène selon leurs associations tout en faisant la promotion de la marque Beelys. Chaque association disposait d’un filtre dédié et l’opération était organisé sous forme de match entre associations. 
+		</p>
+		<h2>Les résultats :</h2>
+		<p><span>Nombre de votes :</span> Plus de 125.000 votes ont été comptabilisés lors de cette opération de 10 jours. L’aspect ludique fonctionne bien et les étudiants souhaitant que leur association soit la mieux classée possible ont offert une visibilité forte à Beelys.
+		<span>Temps passé sur les contenus :</span> Notre outil est branché sur les API Facebook et Google qui remontent un temps de session moyen supérieur à 5 minutes.</p>
 	</section>
 	<?php
 		include('footer.php');
