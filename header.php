904b3cac05bc4bc40255b7e516138ed36266eba0
Thibaut <thibaut.monesma@gmail.com>
2018-03-13 10:24:01 +0100

    add p9

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 		<nav class="kd-nav">
 			<a href="index.php">Accueil</a>
 			<a href="#">Notre Solution</a>
-			<a href="#">Cas d'utilisation</a>
+			<a href="usecase.php">Cas d'utilisation</a>
 <!-- 			<a href="#">Blog</a>
  -->	<a href="form.php">Démo</a>
 		</nav>
